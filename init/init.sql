aad9056ec11a1d27e7f5cd479692e735c1ebfc93
Ramasamy Ramanathan <53431872+RM-RAMASAMY@users.noreply.github.com>
2025-01-29 02:01:04 -0800

    modified products table schema

--- init/init.sql
+++ init/init.sql
@@ -5,9 +5,11 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS products (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(100),
-      price DECIMAL(10, 2)
-    );
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 -- Add more table definitions here
